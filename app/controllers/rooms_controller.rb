abec932243a9555c5ad87a9693d47a7d8f0eeace
KenFukui <79432343+KenFukui@users.noreply.github.com>
2021-03-18 11:17:14 +0900

    チャット画面の編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,7 +7,7 @@ class RoomsController < ApplicationController
   def create
     @room = Room.new(room_params)
     if @room.save
-      redirect_to root_paht
+      redirect_to root_path
     else
       render :new
     end
